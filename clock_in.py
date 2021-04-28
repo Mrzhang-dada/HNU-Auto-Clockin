bf0a3c1f10d27f4126292f89aaaec4019495dcbf
Mrzhang-dada <82631146+Mrzhang-dada@users.noreply.github.com>
2021-04-28 17:14:24 +0800

    Update clock_in.py

--- clock_in.py
+++ clock_in.py
@@ -47,28 +47,30 @@ def main():
     clockin_url = 'https://fangkong.hnu.edu.cn/api/v1/clockinlog/add'
     headers = login()
     lon, lat, real_address = setLocation()
-    clockin_data = {"Temperature":"null",
-                    "RealProvince":args.province,
+    clockin_data = {"InsulatedAddress":"null",
+                    "IsDiagnosis":"0",
+                    "IsInCampus":"1",
+                    "IsInsulated":"0",
+                    "IsNormalTemperature":"null",
+                    "IsSuspected":"0",
+                    "IsTouch":"null",
+                    "IsUnusual":"null",
+                    "IsViaHuBei":"null",
+                    "IsViaWuHan":"null",
+                    "Longitude":lon,
+                    "Latitude":lat,
+                    "ModifyFields":"null"
+                    "MorningTemp":"36"
+                    "RealAddress":real_address,
                     "RealCity":args.city,
                     "RealCounty":args.county,
-                    "RealAddress":real_address,
-                    "IsUnusual":"0",
-                    "UnusualInfo":"",
-                    "IsTouch":"0",
-                    "IsInsulated":"0",
-                    "IsSuspected":"0",
-                    "IsDiagnosis":"0",
-                    "tripinfolist":[{"aTripDate":"","FromAdr":"","ToAdr":"","Number":"","trippersoninfolist":[]}],
-                    "toucherinfolist":[],
+                    "RealProvince":args.province,
+                    "Temperature":"36.5"
+                    "TouchInfo":"null",
+                    "UnusualInfo":"null",
                     "dailyinfo":{"IsVia":"0","DateTrip":""},
-                    "IsInCampus":"1",
-                    "IsViaHuBei":"0",
-                    "IsViaWuHan":"0",
-                    "InsulatedAddress":"",
-                    "TouchInfo":"",
-                    "IsNormalTemperature":"1",
-                    "Longitude":lon,
-                    "Latitude":lat
+                    "tripinfolist":[]
+                    "toucherinfolist":[]
                     }
 
     clockin = requests.post(clockin_url, headers=headers, json=clockin_data)
